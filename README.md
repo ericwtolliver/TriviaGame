66b172b83075ed272c67341bf097a88c7cd4317b
Eric Tolliver <41762913+ericwtolliver@users.noreply.github.com>
2018-09-05 17:39:39 -0400

    Add files via upload
    updated files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TriviaGame
-In this assignment, You'll create a Trivia game using JavaScript for the logic and jQuery to manipulate HTML. Be sure to layout this app with valid HTML and stylish CSS.
+Japan Trivia Game is a web application that allows users to take a timed quiz of Japanese facts.
+
+Technologies Used: HTML, CSS, Javascript, Bootstrap, jQuery
+
+Description:
+
+Japan Trivia Game is a timed trivia game that counts down via intervals. A counter keeps track of how many questions were answered correctly, incorrectly, or not at all. The results are displayed at the end of the quiz. Once the quiz restarts, all counters are reset. The questions are hard coded and will repeat.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -132,18 +132,18 @@ var questionsArray = [
   question10
 ];
 
-// functions
+// Functions
 
 function start() {
   $(".content-div").empty();
-  var startButton = $("<button");
+  var startButton = $("<button>");
   startButton.text("Start");
   startButton.addClass("start btn btn-default answerBtn");
   $(".content-div").append(startButton);
 }
 
 function run() {
-  intervalID = setInterval(decodeURIComponent, 1000);
+  intervalId = setInterval(decrement, 1000);
 }
 
 function decrement() {
@@ -154,10 +154,124 @@ function decrement() {
       setTimeout(function() {
         questionWrite(questionsArray[arrayFinder]);
       }, 2000);
-      solutionWrite(questionArray[arrayFinder]);
+      solutionWrite(questionsArray[arrayFinder]);
+      $(".question-div").html("Incorrect!");
+      stop();
+      unanswered++;
+    } else if (arrayFinder < questionsArray.length) {
+      setTimeout(function() {
+        endWrite(questionsArray[arrayFinder]);
+      }, 2000);
+      solutionWrite(questionsArray[arrayFinder]);
       $(".question-div").html("Incorrect!");
       stop();
       unanswered++;
     }
   }
 }
+
+function stop() {
+  clearInterval(intervalId);
+}
+
+function questionWrite(obj) {
+  time = 20;
+  $(".timer-div").empty();
+  $(".timer-div").html("Time Remaining: " + time + " Seconds");
+  $(".question-div").empty();
+  $(".content-div").empty();
+  run();
+  $(".question-div").html(obj.question);
+  for (var i = 0; i < obj.answers.length; i++) {
+    var answerButton = $("<button>");
+    answerButton.addClass("answer btn btn-default answerBtn");
+    answerButton.text(obj.answers[i]);
+    answerButton.attr("value", obj.values[i]);
+    $(".content-div").append(answerButton);
+    $(".content-div").append("<br>");
+  }
+}
+
+function solutionWrite(obj) {
+  $(".question-div").empty();
+  $(".content-div").empty();
+  $(".content-div").html("The correct answer was " + obj.correct + "<br>");
+  var characterImage = $("<img>");
+  characterImage.attr("height", "250");
+  characterImage.attr("src", obj.image);
+  characterImage.addClass("character");
+  $(".content-div").append(characterImage);
+  arrayFinder++;
+}
+
+function startWrite() {
+  questionWrite(question01);
+}
+
+function answerSelect() {
+  stop();
+  if ($(this).attr("value") == "correct") {
+    solutionWrite(questionsArray[arrayFinder]);
+    $(".question-div").html("Correct!");
+    correct++;
+    if (arrayFinder < questionsArray.length) {
+      setTimeout(function() {
+        questionWrite(questionsArray[arrayFinder]);
+      }, 2000);
+    } else if (arrayFinder < questionsArray.length + 1) {
+      setTimeout(function() {
+        endWrite(questionsArray[arrayFinder]);
+      }, 2000);
+    }
+  } else if ($(this).attr("value") == "incorrect") {
+    solutionWrite(questionsArray[arrayFinder]);
+    $(".question-div").html("Incorrect!");
+    incorrect++;
+    if (arrayFinder < questionsArray.length) {
+      setTimeout(function() {
+        questionWrite(questionsArray[arrayFinder]);
+      }, 2000);
+    } else if (arrayFinder < questionsArray.length + 1) {
+      setTimeout(function() {
+        endWrite(questionsArray[arrayFinder]);
+      }, 2000);
+    }
+  }
+}
+
+function endWrite() {
+  $(".question-div").empty();
+  $(".content-div").empty();
+  $(".question-div").html("Here's how you did!");
+  $(".content-div").html(
+    "<p> Correct: " +
+      correct +
+      "</p>" +
+      "<p> Incorrect: " +
+      incorrect +
+      "</p>" +
+      "<p> Unanswered: " +
+      unanswered +
+      "</p>"
+  );
+  var resetButton = $("<button>");
+  resetButton.addClass("reset btn btn-default answerBtn");
+  resetButton.text("Start Over?");
+  $(".content-div").append(resetButton);
+}
+
+function resetClick() {
+  arrayFinder = 0;
+  incorrect = 0;
+  correct = 0;
+  unanswered = 0;
+  startWrite();
+}
+
+$(document).on("click", ".start", startWrite);
+
+$(document).on("click", ".answer", answerSelect);
+
+$(document).on("click", ".reset", resetClick);
+
+start();
